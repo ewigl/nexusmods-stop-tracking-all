dd9bee6c2d685a1de23a42c900d854ec1069483e
ewigl <ewiglichtl@gmail.com>
2025-08-23 16:09:06 +0800

    added meta info

--- stop-trcking-all.user.js
+++ stop-trcking-all.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name        Stop Tracking All
-// @name:en        Stop Tracking All
+// @name:en     Stop Tracking All
 // @namespace   https://github.com/ewigl/nexusmods-stop-tracking-all
 // @match       https://www.nexusmods.com/mods/trackingcentre*
 // @match       https://www.nexusmods.com/*/mods/trackingcentre*
 // @grant       unsafeWindow
 // @version     1.0
-// @author      -
+// @author      Licht
+// @license     MIT
 // @description 批量取消 Track Nexus Mods 上的模组。
-// @description:en Stop tracking multiple mods at once on Nexus Mods.
+// @description:en  Stop tracking multiple mods at once on Nexus Mods.
 // ==/UserScript==
 
 unsafeWindow.elStopTrackingAll = () => {
